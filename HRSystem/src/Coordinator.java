49aa65723a4a5b7dc82b090f0b83586ecb1fd0e4
dylanli0099 <s3663186@student.rmit.edu.au>
2018-04-08 21:40:42 +1000

    Update Coordinator.java

--- HRSystem/src/Coordinator.java
+++ HRSystem/src/Coordinator.java
@@ -1,6 +1,17 @@
+import java.util.*;
 
 public class Coordinator extends User{
+	
+	ArrayList<Class> classes = new ArrayList<Class>();
 
+	public Coordinator(String name, String id, String phone, char gender)
+	{
+		name = this.name;
+		id = this.id;
+		phone = this.phone;
+		gender = this.gender;
+	}
+	
 	private void ViewCasualWorkers()
 	{
 
@@ -21,8 +32,22 @@ public class Coordinator extends User{
 	
 	}
 
-	public void AddClass()
+	public void AddClass(String name,String time, String date, String tutor, String tutorID)
 	{
+		classes.add(new Class(name, time, date, tutor, tutorID));
 		
+	}
+	
+	
+	public void showClasses()
+	{
+		for(int i=0; i<classes.size();i++)
+		{
+		Class cls = (Class)classes.get(i);
+		cls.getClassName();
+		cls.getDateTime();
+		cls.getTutor();
+		}
+	
 	}
 }
